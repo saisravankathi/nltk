5690ce49d72f6cc74f782a52613e8cb6668d7b5f
Kathi Sai Sravan <saisravankathi.nitk@gmail.com>
2017-10-11 16:31:41 +0530

    GUI Changes
    Changes done to enable select all and enter binding.

--- FAQCreator/guianalyser.py
+++ FAQCreator/guianalyser.py
@@ -12,6 +12,7 @@ class ScrollTxtArea:
         self.createButton(frame)
         self.createClearButton(frame)
         self.textArea(frame)
+        root.bind_class("Text","<Control-a>",self.selectAllTextAreaText)
         
     def textPad(self,frame):
         textPad = Frame(frame)
@@ -39,6 +40,8 @@ class ScrollTxtArea:
         entryBox = Frame(frame)
         self.entry = Entry(entryBox)
         self.entry.pack(side=LEFT)
+        self.entry.bind("<Return>", faqEnterPrint)
+        self.entry.bind("<Control-a>", self.selectAllText)
         entryBox.pack(side=TOP)
     
     def createButton(self,frame):
@@ -54,18 +57,53 @@ class ScrollTxtArea:
         clearbutton.pack(side=TOP)
         
     def getValues(self, clasifier):
+        print("before ques: ",self.textResult.index("end-1c"))
         self.textResult.insert(END,"\nQustion:"+foo.entry.get())
+        self.textResult.tag_add("question_txt",self.textResult.index("end-1c linestart"),self.textResult.index("end-1c"))
+        self.textResult.tag_config("question_txt",foreground="green")
         for v in clasifier.fSet:
+            print("before ans: ",self.textResult.index("end-1c"))
             self.textResult.insert(END,"\nAnswer:"+v)
+            print("after ans: ",self.textResult.index("end-1c"))
             
     def getAlertValues(self):
         self.textResult.insert(END,"\nWarning: Please enter the question.")
 
     def clearResults(self):
         self.textResult.delete(1.0,END)
+    
+    def selectAllText(self,event):
+        # select text after 50ms
+        root.after(50, self.select_all, event.widget)
+
+    def select_all(self,widget):
+        # select text
+        widget.select_range(0, 'end')
+        # move cursor to the end
+        widget.icursor('end')
+        
+    def selectAllTextAreaText(self, event):
+        event.widget.tag_add("sel","1.0","end")
             
 
 def faqPrint():
+    #Providing training data to the classifier.
+    scriptpath = os.path.dirname(__file__)
+    filename_when = os.path.join(scriptpath,'when.txt')
+    filename_where = os.path.join(scriptpath, 'where.txt')
+    filename_who = os.path.join(scriptpath, 'who.txt')
+
+    whenText = open(filename_when, 'r')
+    whereText = open(filename_where, 'r')
+    whoText = open(filename_who, 'r')
+    if foo.entry.get() != '':
+        c = Classifier(whenText,whereText,whoText,foo.text.get("1.0",END),foo.entry.get(),dict())
+        foo.getValues(c)
+    else:
+        foo.getAlertValues()
+    foo.entry.delete(0,END)
+    
+def faqEnterPrint(event):
     #Providing training data to the classifier.
     scriptpath = os.path.dirname(__file__)
     filename_when = os.path.join(scriptpath,'when.txt')
